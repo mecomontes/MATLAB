acfd9746a42bd1f0ad6a86b18822f9cea849900a
Robinson Montes Gómez <58984966+mecomontes@users.noreply.github.com>
2021-07-10 17:05:55 -0500

    Delete Análisis Farmacinetico directory

--- Análisis Farmacinetico/monocompartimental/imipenem.m
+++
@@ -1,5 +0,0 @@
-function F=imipenem(x)
-
-t=load('tiempo.txt');%datos de tiempo experimental [min]
-Cs=load('concentracion.txt'); %datos experimentales de concentracion [ug/mL]
-F=Cs-(x(1).*exp(-x(2).*t));
